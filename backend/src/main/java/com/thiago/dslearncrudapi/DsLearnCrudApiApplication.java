6025ec6d5570f1bf1d9b93ea87b4948ed4fdf256
Thiago Martins <devthiagomartins@gmail.com>
2022-01-19 19:26:30 -0300

    Project created

---
+++ backend/src/main/java/com/thiago/dslearncrudapi/DsLearnCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.dslearncrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsLearnCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsLearnCrudApiApplication.class, args);
+	}
+
+}
