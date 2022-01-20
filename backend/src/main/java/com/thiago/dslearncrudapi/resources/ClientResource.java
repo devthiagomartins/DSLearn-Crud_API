df4148ac05fef03ac485285f8a76d32aeceff41c
Thiago Martins <devthiagomartins@gmail.com>
2022-01-19 21:51:24 -0300

    CRUD complete with no Exceptions

--- backend/src/main/java/com/thiago/dslearncrudapi/resources/ClientResource.java
+++ backend/src/main/java/com/thiago/dslearncrudapi/resources/ClientResource.java
@@ -1,11 +1,23 @@
 package com.thiago.dslearncrudapi.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.thiago.dslearncrudapi.dto.ClientDTO;
 import com.thiago.dslearncrudapi.services.ClientService;
 
@@ -21,5 +33,37 @@ public class ClientResource {
 		ClientDTO dto = clientService.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAllPaged(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);	
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = clientService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+		clientService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+		dto = clientService.update(id,dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
 
 }
--- backend/src/main/java/com/thiago/dslearncrudapi/services/ClientService.java
+++ backend/src/main/java/com/thiago/dslearncrudapi/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.thiago.dslearncrudapi.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.thiago.dslearncrudapi.dto.ClientDTO;
+import com.thiago.dslearncrudapi.entities.Client;
 import com.thiago.dslearncrudapi.repositories.ClientRepository;
 
 @Service
@@ -12,12 +15,47 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository clientRepository;
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		return new ClientDTO(clientRepository.findById(id).get());
 	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = clientRepository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client client = new Client();
+		convertDtoToEntity(dto, client);
+		client = clientRepository.save(client);
+		return new ClientDTO(client);
+	}
+
+
+	public void deleteById(Long id) {
+		clientRepository.deleteById(id);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		Client client = clientRepository.getOne(id);
+		convertDtoToEntity(dto, client);
+		client = clientRepository.save(client);
+		return new ClientDTO(client);
+	}
 	
 	
+	
+	private void convertDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity.setIncome(dto.getIncome());
+		entity.setCpf(dto.getCpf());
+	}
 
 }
